6e350e1a425adcebc8445a9b5a61c59e3a6b01a1
Rafasashi\Raphael <raphael.dartigues@gmail.com>
2017-01-26 11:52:01 +0100

    remove dev folders

--- views-dev/editor.php
+++
@@ -1,3 +0,0 @@
-<div class="loadingIframe" style="width: 100%;position: relative;background-position: 50% center;background-repeat: no-repeat;background-image:url('<?php echo $this->server->url; ?>/c/p/live-template-editor-server/assets/loader.gif');height:64px;"></div>
-
-<iframe id="editorIframe" src="<?php echo $_SERVER['SCRIPT_URI'] . '?uri=' . $this->layer->uri . '&lk=' . md5( 'layer' . $this->layer->uri . $this->_time ) . '&_=' . $this->_time; ?>" style="margin-top: -65px;position: relative;width: 100%;top: 0;bottom: 0;border:0;height: 1200px;overflow: hidden;"></iframe>
--- views-dev/upgrade.php
+++
@@ -1,5 +0,0 @@
-<div class="alert alert-warning">
-	
-	This template is not included in your plan...
-
-</div>
