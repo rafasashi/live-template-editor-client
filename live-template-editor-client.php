994c8c30b0b7184fd6bbc05f691fb48fb271ac7c
Rafasashi\Raphael <raphael.dartigues@gmail.com>
2021-03-03 10:42:26 +0545

    version

--- live-template-editor-client.php
+++ live-template-editor-client.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Live Template Editor Client
- * Version: 1.1.6.69
+ * Version: 1.1.7.0
  * Plugin URI: https://github.com/rafasashi
  * Description: Live Template Editor allows you to edit and save HTML5 and CSS3 templates.
  * Author: Rafasashi
