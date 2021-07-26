d886a121f5f203997f67030381f882fbd001c851
Rafasashi\Raphael <raphael.dartigues@gmail.com>
2021-07-26 17:08:16 +0545

    request ip fixed

--- includes/lib/class-ltple-client-request.php
+++ includes/lib/class-ltple-client-request.php
@@ -5,9 +5,13 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 class LTPLE_Client_Request {
 
 	var $parent;
-	var $url;
+	
+	var $proto = 'https://';
+	var $is_remote;
+	var $ip;
+	var $user_agent;
+	var $ref_key;
 	var $ref_id;
-	var $proto = 'http://';
 
 	/**
 	 * Constructor function
@@ -36,7 +40,11 @@ class LTPLE_Client_Request {
 			
 			$this->is_remote = true;
 		}
-
+		
+		//get user ip
+		
+		$this->ip = $this->get_user_ip();	
+		
 		// get user agent
 		
 		$this->user_agent = $_SERVER['HTTP_USER_AGENT'];
@@ -90,4 +98,21 @@ class LTPLE_Client_Request {
 			}
 		});
 	}
+	
+	public function get_user_ip() {
+		
+		foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
+			
+			if (array_key_exists($key, $_SERVER) === true){
+				
+				foreach (array_map('trim', explode(',', $_SERVER[$key])) as $ip){
+					
+					if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false){
+						
+						return $ip;
+					}
+				}
+			}
+		}
+	}	
 }
